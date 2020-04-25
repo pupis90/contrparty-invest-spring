73f2978727904bcc164dbfe68fe21009a6e7dd7b
pupis90 <victor.golovenkov@spi2.ru>
2020-04-25 10:00:06 +0300

    Поправил IDENTITY на AUTO

--- resume-data/src/main/java/security/contrparties/investigations/entities/BaseEntity.java
+++ resume-data/src/main/java/security/contrparties/investigations/entities/BaseEntity.java
@@ -10,7 +10,7 @@ import javax.persistence.MappedSuperclass;
 public class BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
 
